13e7d47e1c3ec3f00a8a3c868ff2976d9da3a482
Bruno Araujo <baraujo@cobasi.net>
2019-04-25 16:42:10 -0300

    moveimentos dos misseis

--- animation.js
+++ animation.js
@@ -1,7 +1,6 @@
 window.onkeydown = function(e) {myFunction(e)};
 function myFunction(e) {
     var ball = document.getElementById("ball");
-    console.log(ball.offsetLeft);
     var distance = 50;
     switch (e.keyCode) {
         case 38:
@@ -23,4 +22,48 @@ function myFunction(e) {
         default:
             break;
     }
+}
+$(document).ready(function(){
+    
+    myApp();
+});
+
+function myApp() {
+    const _this = myApp;
+    const dificult = 0;
+    const time = 2000;
+
+    _this.init = () => {
+        _this.putBulets(0);
+        _this.downBullets();
+    }
+
+    _this.putBulets = (i) => {
+        setTimeout(function () {
+            $("body").append("<div style=\"left: "+_this.getRandonLeft()+"px;\" data-bulet=\""+i+"\" class=\"bullet\"></div>");
+            i++;
+            _this.putBulets(i);
+        }, time)
+    }
+
+    _this.downBullets = () => {
+
+        setTimeout(function () {
+            var heigth = $(window).height();
+            $("[data-bulet]").each(function (params) {
+                var top = $(this).position().top;
+                $(this).offset({top:(top+50)});
+                if( (top+50) > (heigth-30) )
+                    $(this).remove();
+            });
+
+            _this.downBullets();
+        }, 500);
+    }
+
+    _this.getRandonLeft = () => {
+        return Math.floor(Math.random() * ( $( window ).width() - 50) );
+    }
+
+    _this.init();
 }
